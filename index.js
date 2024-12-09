1ace4090ba5e5ec6fe5842f0253ec4c21042dab4
Nikhil <Nikhil@glocify.com>
2024-12-09 17:39:04 +0530

    curreently working with ios notificaiton not checked

--- index.js
+++ index.js
@@ -5,27 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import PushNotification from "react-native-push-notification";
-import { navigationRef } from './app/navigation/NavigationRef';
-PushNotification.configure({
 
-    onNotification: function (notification) {
-        console.log("NOTIFICATION:", notification);
-    
-        // process the notification
-        const navigateToNotificationScreen = (notificationData) => {
-            // Navigate to Notification screen with notification data
-            if (navigationRef.isReady()) {
-                navigationRef.navigate('RunScan', { notification });
-              }
-          };
-      
-          navigateToNotificationScreen(notification);
-        // (required) Called when a remote is received or opened, or local notification is opened
-        // notification.finish(PushNotificationIOS.FetchResult.NoData);
-      },
-    
-    requestPermissions: true,
-    requestPermissions: Platform.OS === 'ios'
-})
+import { navigationRef } from './app/navigation/NavigationRef';
+
 AppRegistry.registerComponent(appName, () => App);
