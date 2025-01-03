e9c348233933698f506cd06af3b33e4ec8546f16
Nikhil <Nikhil@glocify.com>
2025-01-03 18:01:41 +0530

    left icon svg strokewidth

--- app/components/ui/svgIcons/LeftCircleIcon.tsx
+++ app/components/ui/svgIcons/LeftCircleIcon.tsx
@@ -17,7 +17,7 @@ const LeftCircleIcon = (props: svgProps) => {
         r="48" // Radius (adjust for thickness)
         fill="white" // Fill inside the circle
         stroke="black" // Outer circle border color
-        strokeWidth="4" // Border thickness
+        strokeWidth="6" // Border thickness
       />
 
       <G>
@@ -26,7 +26,9 @@ const LeftCircleIcon = (props: svgProps) => {
           d="M44.942,50.412l14.037-15.487c0.742-0.818,0.68-2.083-0.139-2.824c-0.817-0.742-2.083-0.679-2.824,0.139L40.784,49.044
        c-0.409,0.451-0.565,1.038-0.493,1.598c-0.016,0.564,0.196,1.131,0.647,1.539L57.74,67.412c0.383,0.348,0.864,0.519,1.344,0.519
        c0.545,0,1.087-0.222,1.482-0.657c0.741-0.818,0.68-2.083-0.139-2.824L44.942,50.412z"
-          fill="black" // Fill for the backward arrow
+          fill="black"
+          stroke="black" // Outer circle border color
+        strokeWidth="3" // Fill for the backward arrow
         />
       </G>
     </Svg>
